d219e6b8093f323d407633c771957a637e4152b8
Prashant <nelluri.p@gmail.com>
2018-01-09 18:20:28 +0530

    Basic examples on Executor framework

---
+++ src/main/java/Executor_Basic/Executor_Basic/App.java
@@ -0,0 +1,13 @@
+package Executor_Basic.Executor_Basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
