7bb19feb08a6f0b8c09941464f921a59aa50453c
Prashant <nelluri.p@gmail.com>
2018-01-10 11:37:23 +0530

    Java 8 features

--- src/main/java/Executor_Basic/Executor_Basic/ExecutorServiceExample.java
+++ src/main/java/Executor_Basic/Executor_Basic/ExecutorServiceExample.java
@@ -1,24 +1,56 @@
 package Executor_Basic.Executor_Basic;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ExecutorServiceExample {
 	public static void main(String[] args) {
 		System.out.println("Start");
 		ExecutorService es = Executors.newFixedThreadPool(4);
 		es.submit(new MyTask("one"));
-		es.submit(ExecutorServiceExample :: get);
-		es.submit(new MyTask("two"));
-		es.submit(new MyTask("three"));
+		//Runnable
+		//callable
+		//Runnable with parameter returns the same
+		//Returns value
+		Future<Integer> object = es.submit(ExecutorServiceExample :: get);
+		try {
+			waitForValue(object.get());
+			System.out.println("after get");
+			System.out.println(object.get());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		// execute will not return anything
+		//submit will return Future object
+		//returns nothing
+		es.execute(ExecutorServiceExample :: get);
+		
+		//es.submit(new MyTask("two"));
+		//es.submit(new MyTask("three"));
 		es.shutdown();
 		System.out.println("End");
 	}
 
-	public static void get()
+	public static int get()
 	{
-		System.out.println("simha");
+		int i=0;
+		for(;i<200;i++) {
+			System.out.println("without param"+i);
+		}
+		return i;
+		
+	}
+	public static int waitForValue(int i)
+	{
+		for(;i<250;i++) {
+			System.out.println("with param"+i);
+		}
+		return 20;
 		
 	}
 }
